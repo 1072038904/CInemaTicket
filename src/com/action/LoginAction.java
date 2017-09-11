eb04bf3a407cce7a88bffa22715d8a774f1c2d7b
1072038904 <1072038904@qq.com>
2017-09-11 22:09:21 +0800

    零食系统

--- src/com/action/LoginAction.java
+++ src/com/action/LoginAction.java
@@ -3,14 +3,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.model.Account;
-import com.opensymphony.xwork2.ActionSupport;
 import com.service.LoginService;
 @Controller
-public class LoginAction extends ActionSupport{
-	/**
-	 * 
-	*/
-	private static final long serialVersionUID = 3707624644418852546L;
+public class LoginAction{
+
 	@Autowired
 	private LoginService loginService;
 	private Account account;
@@ -28,19 +24,19 @@ public class LoginAction extends ActionSupport{
 	public void setLoginService(LoginService loginService) {
 		this.loginService = loginService;
 	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
+
 	public void setAccount(Account account) {
 		this.account = account;
 	}
 	public LoginAction() {
 	}
+	public String execute() throws Exception{
+		return "error";
+	}
 	public String login()throws Exception{
 		System.out.println(loginService.IsValid(account));
 		if(loginService==null)
 		{
-			System.out.println("2312");
 			return "eorror";
 		}
 		if(loginService.IsValid(account)==1)
